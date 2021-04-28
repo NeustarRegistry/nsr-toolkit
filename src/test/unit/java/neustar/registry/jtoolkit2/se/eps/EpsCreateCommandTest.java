9da029b763bdf215fc9988fb4312d5e55450bfd1
Richard Cane <richard@registry.godaddy>
2021-04-28 17:48:35 +1000

    RPE-2893: Change datatype for IPR Number for test

--- src/test/unit/java/neustar/registry/jtoolkit2/se/eps/EpsCreateCommandTest.java
+++ src/test/unit/java/neustar/registry/jtoolkit2/se/eps/EpsCreateCommandTest.java
@@ -48,7 +48,7 @@ public class EpsCreateCommandTest {
     @Test
     public void shouldCreateCommandWithRequiredFieldsAndIntellectualPropertyId() {
         Command cmd = new EpsCreateCommand(EpsType.standard, Collections.singletonList("jtkutest"), "jtkUT3st",
-                "registrant", new Period(1), 3111246);
+                "registrant", new Period(1), "3111246");
         try {
             String xml = cmd.toXML();
             assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\""
